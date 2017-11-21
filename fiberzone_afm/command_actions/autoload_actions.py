060a61a41de2a5bfa30b2168bd737640b359aa4e
Yaroslav Nikonorov <yaroslav.n@quali.com>
2017-11-21 16:37:32 +0200

    Fixed parse tablke

--- fiberzone_afm/command_actions/autoload_actions.py
+++ fiberzone_afm/command_actions/autoload_actions.py
@@ -78,8 +78,8 @@ class AutoloadActions(object):
     def _parse_table(data, pattern):
         compiled_pattern = re.compile(pattern, re.IGNORECASE)
         table = []
-        for record in data.split('\r\n'):
-            matched = re.search(compiled_pattern, record)
+        for record in data.split('\n'):
+            matched = re.search(compiled_pattern, record.strip())
             if matched:
                 table.append(re.split(r'\s+', matched.group(0)))
         return table

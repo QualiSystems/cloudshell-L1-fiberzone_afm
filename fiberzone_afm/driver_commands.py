075f3b5e7177eeada9b00268a50381976e3f97c2
Yaroslav Nikonorov <yaroslav.n@quali.com>
2017-11-23 22:04:34 +0200

    Changed cli engine

--- fiberzone_afm/driver_commands.py
+++ fiberzone_afm/driver_commands.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os
 import time
 
 from cloudshell.layer_one.core.driver_commands_interface import DriverCommandsInterface
 from cloudshell.layer_one.core.helper.runtime_configuration import RuntimeConfiguration
 from cloudshell.layer_one.core.response.response_info import GetStateIdResponseInfo, ResourceDescriptionResponseInfo, \
     AttributeValueResponseInfo
+from fiberzone_afm.cli.fiberzone_cli_handler import FiberzoneCliHandler
 from fiberzone_afm.command_actions.autoload_actions import AutoloadActions
 from fiberzone_afm.command_actions.mapping_actions import MappingActions
 from fiberzone_afm.helpers.autoload_helper import AutoloadHelper
-from fiberzone_afm.helpers.test_cli import TestCliHandler
 
 
 class DriverCommands(DriverCommandsInterface):
@@ -25,9 +24,9 @@ class DriverCommands(DriverCommandsInterface):
         :type logger: logging.Logger
         """
         self._logger = logger
-        # self._cli_handler = FiberzoneCliHandler(logger)
-        self._cli_handler = TestCliHandler(
-            os.path.join(os.path.dirname(__file__), 'helpers', 'test_fiberzone_data'), logger)
+        self._cli_handler = FiberzoneCliHandler(logger)
+        # self._cli_handler = TestCliHandler(
+        #     os.path.join(os.path.dirname(__file__), 'helpers', 'test_fiberzone_data'), logger)
 
         self._mapping_timeout = RuntimeConfiguration().read_key('MAPPING.TIMEOUT')
 
